2954ccc72a1b9f7dab042fcfd01f3f5c6d4eb558
unknown <naya.altahan00@gmail.com>
2020-10-31 20:40:37 +0100

    ss

--- mix1-assignment3-main/Second VR Project/Assets/Scripts/CubeBehaviour.cs
+++ mix1-assignment3-main/Second VR Project/Assets/Scripts/CubeBehaviour.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CubeBehaviour : MonoBehaviour
 {
+    private Rigidbody _rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += transform.forward * (Time.deltaTime * 2);
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!other.collider.CompareTag(tag))
+        {
+            _rigidbody.isKinematic = true;
+        }
+ 
     }
 }
--- mix1-assignment3-main/Second VR Project/Assets/Scripts/SaberScript.cs
+++ mix1-assignment3-main/Second VR Project/Assets/Scripts/SaberScript.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
 
 public class SaberScript : MonoBehaviour
 {
-    public LayerMask layer;
     private Vector3 previousPos;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,13 +18,25 @@ public class SaberScript : MonoBehaviour
     void Update()
     {
         RaycastHit hit;
-        if(Physics.Raycast(transform.position, transform.forward, out hit, 1, layer))
+        if(Physics.Raycast(transform.position, transform.forward, out hit) //&& (hit.transform.gameObject == transform.gameObject)
+                                                                           )
         {
-            if (Vector3.Angle(transform.position - previousPos, hit.transform.up) > 130)
-            {
+           if (Vector3.Angle(transform.position - previousPos, hit.transform.up) > 130 && hit.collider.CompareTag(tag))
+           {
                 Destroy(hit.transform.gameObject);
-            }
+                Debug.Log("111");
+           }
         }
         previousPos = transform.position;
     }
+
+    //  private void OnCollisionEnter(Collision col)
+    // {
+    //     Debug.Log("323");
+    //     if ( col.collider.CompareTag(tag) )
+    //     {
+    //         Destroy(col.transform.gameObject);
+    //         Debug.Log("111");
+    //     }
+    // }
 }
