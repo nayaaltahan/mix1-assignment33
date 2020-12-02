5ace749096385f3a3986bb750b4a60a1ec157fa9
naya <naya.altahan00@gmail.com>
2020-12-02 14:46:43 +0100

    its the final clean up

--- mix1-assignment3-main/Second VR Project/Assets/Scripts/SaberScript.cs
+++ mix1-assignment3-main/Second VR Project/Assets/Scripts/SaberScript.cs
@@ -18,25 +18,16 @@ public class SaberScript : MonoBehaviour
     void Update()
     {
         RaycastHit hit;
-        if(Physics.Raycast(transform.position, transform.forward, out hit) //&& (hit.transform.gameObject == transform.gameObject)
+        if(Physics.Raycast(transform.position, transform.forward, out hit) 
                                                                            )
         {
            if (Vector3.Angle(transform.position - previousPos, hit.transform.up) > 130 && hit.collider.CompareTag(tag))
            {
                 Destroy(hit.transform.gameObject);
-                Debug.Log("111");
            }
         }
         previousPos = transform.position;
     }
 
-    //  private void OnCollisionEnter(Collision col)
-    // {
-    //     Debug.Log("323");
-    //     if ( col.collider.CompareTag(tag) )
-    //     {
-    //         Destroy(col.transform.gameObject);
-    //         Debug.Log("111");
-    //     }
-    // }
+    
 }
